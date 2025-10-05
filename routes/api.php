9ce41a909f4743f6f4b1c11ce2792685ffca9db2
Annisa Mutia Rahman <annisar496@gmail.com>
2025-10-05 01:00:30 +0000

    update: api.php

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get('/test', function() {
+    return response()->json(['message' => 'API is working']);
+});
 
 Route::post('/register-user', [UserController::class, 'register']);
 Route::post('/login-user', [UserController::class, 'login']);
