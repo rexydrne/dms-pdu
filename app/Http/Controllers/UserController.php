1e8251e813f3f9f0d662b4a12517c8a907ec3a4d
felcy <husnafelisacahyani@gmail.com>
2025-11-14 15:45:08 +0000

    edit: UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -71,7 +71,7 @@ class UserController extends Controller
                 $user->sendEmailVerificationNotification();
 
                 try {
-                    $user->assignRole('admin');
+                    $user->assignRole('admin', 'api');
                 } catch (RoleDoesNotExist $e) {
                     throw new \Exception('Role "admin" does not exist.' . $e->getMessage());
                 }
