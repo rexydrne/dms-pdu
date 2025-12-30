4f252553babf548d7776bd92ede54d0e2a2da1be
annisa-ugm <annisar496@gmail.com>
2025-12-30 11:43:07 +0700

    update: model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use Illuminate\Auth\MustVerifyEmail;
+// use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Database\Eloquent\BroadcastsEvents;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -31,10 +31,10 @@ use Spatie\Permission\Traits\HasRoles;
  *     @OA\Property(property="updated_at", type="string", format="date-time")
  * )
  */
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles, HasApiTokens, MustVerifyEmail, Searchable, BroadcastsEvents, Dispatchable, InteractsWithSockets, SerializesModels;
+    use HasFactory, Notifiable, HasRoles, HasApiTokens, Searchable, BroadcastsEvents, Dispatchable, InteractsWithSockets, SerializesModels;
 
     /**
      * The attributes that are mass assignable.
